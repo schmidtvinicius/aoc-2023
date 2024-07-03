f8824b2f6c399bf9d247cd1371f05584e75e5a1f
Vinicius Schmidt <vinimachado.s@protonmail.com>
2024-07-03 17:02:09 +0200

    Finished day 1

--- src/aoc_2023/day1/1.clj
+++ src/aoc_2023/day1/1.clj
@@ -5,6 +5,6 @@
   (let [digits (remove str/blank? (re-seq #"\d" line))]
     (apply str (first digits) (take-last 1 digits))))
 
-(let [input (slurp "/mnt/HDD/programming/advent-of-code-2023/day1/input.txt")]
+(let [input (slurp "resources/day1/input.txt")]
   (reduce +
           (map #(Integer/parseInt %) (map concat-first-and-last-digits (str/split-lines input)))))
--- src/aoc_2023/day1/2.clj
+++ src/aoc_2023/day1/2.clj
@@ -5,9 +5,10 @@
   (let [digits (remove str/blank? (re-seq #"\d" line))]
     (apply str (first digits) (take-last 1 digits))))
 
-(let [input (str/replace (slurp "/mnt/HDD/programming/advent-of-code-2023/day1/input.txt")
-                         #"one|two|three|four|five|six|seven|eight|nine"
-                         {"one" "1" "two" "2" "three" "3" "four" "4" "five" "5" "six" "6" "seven" "7" "eight" "8" "nine" "9"})]
+(let [input (str/replace (slurp "resources/day1/input.txt")
+                         #"oneight|twone|eightwo|one|two|three|four|five|six|seven|eight|nine"
+                         {"oneight" "18" "twone" "21" "eightwo" "82"
+                          "one" "1" "two" "2" "three" "3" "four" "4" "five" "5" "six" "6" "seven" "7" "eight" "8" "nine" "9"})]
   (->>
    (str/split-lines input)
    (map concat-first-and-last-digits)
